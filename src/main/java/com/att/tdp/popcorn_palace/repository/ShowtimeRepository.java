c8415761eeb0144978dd42d741a3aaed8b344268
ShayMashiah <Shay2908@gmail.com>
2025-03-21 19:08:03 +0200

    feat: add GET /showtimes/ and /showtimes/{showtimeId} endpoints to present all shotimes or by showtimeId and add POST /showtimes/ to add a new showtime only if movie exist

---
+++ src/main/java/com/att/tdp/popcorn_palace/repository/ShowtimeRepository.java
@@ -0,0 +1,7 @@
+package com.att.tdp.popcorn_palace.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.att.tdp.popcorn_palace.entity.Showtime;
+
+public interface ShowtimeRepository extends JpaRepository<Showtime, Long> {
+}
