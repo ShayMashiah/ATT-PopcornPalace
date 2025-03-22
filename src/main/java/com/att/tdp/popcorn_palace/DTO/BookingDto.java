f6b552511743ba9a5c81553465e4ca75159890b2
ShayMashiah <Shay2908@gmail.com>
2025-03-22 21:13:49 +0200

    feat: add validation annotations to BookingDTO

--- src/main/java/com/att/tdp/popcorn_palace/DTO/BookingDto.java
+++ src/main/java/com/att/tdp/popcorn_palace/DTO/BookingDto.java
@@ -1,8 +1,16 @@
 package com.att.tdp.popcorn_palace.DTO;
 
+import jakarta.validation.constraints.*;
+
 public class BookingDto {
+    @NotNull(message = "Showtime ID is required")
     public Long showtimeId;
+
+    @NotNull(message = "Seat number is required")
+    @Positive(message = "Seat number must be greater than 0")
     public Integer seatNumber;
+
+    @NotBlank(message = "User ID is required")
     public String userId;
 
     public BookingDto(Long showtimeId, Integer seatNumber, String userId) {
